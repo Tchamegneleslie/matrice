58ae3612f05a58cdcd3a721d25b1c71cb8885491
Tchamegneleslie <kamenrosine@gmail.com>
2024-11-01 01:06:55 +0100

    add3.h

---
+++ add3.h
@@ -0,0 +1,15 @@
+#ifndef ADD3_H_INCLUDED
+#define ADD3_H_INCLUDED
+
+// declaration des prototypes des fonctions des matrices d'ordre 3
+
+
+void sommeMatrice3 ( int  matC[3][3]  , int  matD[3][3] ) ;
+void soustractionMat3( float matC1[3][3], float matD1[3][3] );
+void multiMat3 ( float matC3[3][3], float matD3[3][3] ) ;
+float determinant3 ( float matE1[3][3] );
+void inverseMat3 ( float matF1[3][3] ) ;
+void transpoMat3 ( float matG1[3][3]  ) ;
+float traceMat3 (float matH1[3][3] ) ;
+
+  #endif 
