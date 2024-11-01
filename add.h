e8a55229a7240b6a1264b83ba12a723d45ab6e8a
Tchamegneleslie <kamenrosine@gmail.com>
2024-11-01 01:06:13 +0100

    add.h

---
+++ add.h
@@ -0,0 +1,15 @@
+#ifndef ADD_H_INCLUDED
+#define ADD_H_INCLUDED
+
+// declaration des prototypes des fonctions des matrices d'ordre 2
+
+
+void sommeMatrice2(float matA [2][2], float matB [2][2] ) ;
+void soustractionMat2( float matA1 [2][2], float matB1 [2][2] );
+void multiMat2 ( float matA3[2][2], float matB3[2][2] ) ;
+float determinant ( float matE[2][2] );
+void inverseMat2 ( float matF[2][2] ) ;
+void transpoMat2 ( float matG[2][2] ) ;
+float traceMat2 (float matH[2][2] ) ;
+
+    #endif 
